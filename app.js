92466408679d45a0d50b061beaad80765cf868d8
Miguel Reyes <miguel.reyes@enernoc.com>
2018-03-23 13:02:40 -0400

    Encoding String and using yargs to read flag.
    npm install yargs@11.0.0 --save
    node app.js -a '1301 lombard st'

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+      a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+      }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(encodeURIComponent(argv.a));
+
+var address = encodeURIComponent(argv.a);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20Lombard%20Street%20philadelphia&key=AIzaSyCxb1MxWWO90aVXiWfSqtF7TeyAM_c4uXY',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCxb1MxWWO90aVXiWfSqtF7TeyAM_c4uXY`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);

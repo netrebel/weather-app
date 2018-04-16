8eb9c09a19a640fde109ebb911c2dcf5a47c7725
Miguel Reyes <miguel.reyes@enernoc.com>
2018-04-16 12:56:04 -0400

    Async method using a promise

--- playground/promise.js
+++ playground/promise.js
@@ -1,13 +1,31 @@
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // Either resolve or reject, can't do both. They cna only be called once!
-    // resolve('Hey, it worked!');
-    reject('Unable to fulfill promise');
-  }, 2500);
+var asyncAdd = (a, b) => {
+  return new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      if(typeof a === 'number' &&  typeof b === 'number'){
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+  });
+};
+
+asyncAdd(5, 7).then((res) => {
+  console.log('Results:', res);
+}, (errorMessage) => {
+  console.log(errorMessage);
 });
 
-somePromise.then((message) => {
-  console.log('Success: ', message);
-}, (errorMessage) => {
-  console.log('Error: ', errorMessage);
-});
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // Either resolve or reject, can't do both. They cna only be called once!
+//     // resolve('Hey, it worked!');
+//     reject('Unable to fulfill promise');
+//   }, 2500);
+// });
+//
+// somePromise.then((message) => {
+//   console.log('Success: ', message);
+// }, (errorMessage) => {
+//   console.log('Error: ', errorMessage);
+// });
